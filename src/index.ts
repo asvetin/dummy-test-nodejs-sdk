d2813e3ccc1adb690761d572b21c7afab41a1502
asvetin <anton.svetin@gmail.com>
2021-10-26 15:41:05 +0200

    fix(code): Changing var name

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ export function returnANumber(): number {
   return 1
 }
 
-export function doSumMath(x: number, y: number): number {
-  return x + y
+export function doSumMath(x: number, z: number): number {
+  return x + z
 }
